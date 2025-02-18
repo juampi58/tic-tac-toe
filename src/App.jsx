8b39a828a0b1de150f690d205a5184a7140a9521
juampi58 <juanpabloloza@gmail.com>
2025-02-18 12:19:38 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [board, setBoard] = useState(Array(9))
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+
+
+export default App
